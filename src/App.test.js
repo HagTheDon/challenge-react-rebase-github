7b6703abed02754aeccdb11bad381560a3133012
Alex Hughes <alexhughes@AlexMBPro.local>
2021-05-06 14:09:10 -0500

    feat: add Auto Review article

--- src/App.test.js
+++ src/App.test.js
@@ -6,9 +6,23 @@ test('renders My really cool blog site logo', () => {
   const linkElement = screen.getByText(/My really cool blog site/i);
   expect(linkElement).toBeInTheDocument();
 });
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> f5118e3... feat: add Auto Review article
 
 test('renders Biology Today article', () => {
   render(<App />);
   const linkElement = screen.getByText(/Biology Today:/i);
+<<<<<<< HEAD
   expect(linkElement).toBeInTheDocument();
 });
+=======
+test('renders Auto Review Article', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Auto Review:/i);
+  expect(linkElement).toBeInTheDocument();
+});
+>>>>>>> b7f6f32... feat: add Auto Review article
+>>>>>>> f5118e3... feat: add Auto Review article
